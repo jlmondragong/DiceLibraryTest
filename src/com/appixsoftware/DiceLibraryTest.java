a141f0137c56b40586e904c15dae26e84431853b
joseluis-mondragon <joseluis-mondragon@joseluismondragon-HP-ENVY-m4-Notebook-PC>
2018-01-11 23:46:41 -0600

    Update

--- src/com/appixsoftware/DiceLibraryTest.java
+++ src/com/appixsoftware/DiceLibraryTest.java
@@ -2,6 +2,7 @@ package com.appixsoftware;
 
 import com.appixsoftware.DiceLibrary.Dice;
 import com.appixsoftware.DiceLibrary.DiceThrower;
+import com.appixsoftware.DiceLibrary.DiceType;
 
 /**
  *
@@ -14,18 +15,18 @@ public class DiceLibraryTest {
      */
     public static void main(String[] args) {
         DiceThrower dt = new DiceThrower();
-        int sidesNumber = 8;
-        int[] s = new int[sidesNumber];
-        Dice[] dices = dt.getThrow(sidesNumber, 100);
+        DiceType diceType = DiceType.D8;
+        int[] s = new int[diceType.getSides()];
+        Dice[] dices = dt.getThrow(diceType, 10);
         for (Dice dice : dices) {
-            s[dice.getCurrentValue()-1]++;
+            s[dice.getCurrentValue() - 1]++;
             System.out.println(dice);
         }
 
         System.out.println("");
         System.out.println("Sumary:");
         for (int i = 0; i < s.length; i++) {
-            System.out.println((i+1)+": "+s[i]);
+            System.out.println((i + 1) + ": " + s[i]);
         }
 
     }
